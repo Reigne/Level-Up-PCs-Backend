241f5451bff5cda33903f08f3003d17a3c4f6f86
Reigne <monteronaelija@yahoo.com>
2024-05-31 22:10:09 +0800

    fix cors

--- app.js
+++ app.js
@@ -11,16 +11,32 @@ const products = require("./routes/product");
 
 var cors = require("cors");
 
-app.use(
-  cors({
-    origin: `${process.env.FRONTEND_URL}`,
-    credentials: true,
-  })
-);
+const allowedOrigins = [`${process.env.FRONTEND_URL}`];
+
+// app.use(
+//   cors({
+//     origin: `${process.env.FRONTEND_URL}`,
+//     credentials: true,
+//   })
+// );
 
 app.use(cookieParser());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use("/api/v1", user);
 app.use("/api/v1", categories);
 app.use("/api/v1", brands);
