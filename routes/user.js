ebf5102a3da208500280e200979ed50ce4896280
Reigne <monteronaelija@yahoo.com>
2024-06-03 21:52:57 +0800

    add route for login

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../utils/multer");
 
-const { registerUser } = require("../controllers/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
+const { register, login, profile } = require("../controllers/userController");
 
-router.post("/register", upload.single("avatar"), registerUser);
+router.post("/register", upload.single("avatar"), register);
+router.post("/login", login);
+router.get("/profile", isAuthenticatedUser, profile);
 
 module.exports = router;
